5f9ba74a3e0a46b4eff23e3a6efc780569e068a9
qwertyhero <jrmkim50@gmail.com>
2017-07-17 00:45:28 -0400

    Updated Timer App for iOS
    Made with swift

--- MyEggTimer/ViewController.swift
+++ MyEggTimer/ViewController.swift
@@ -10,9 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    var timer = Timer()
+    var time = 210
+    
+    func decreaseTimer () {
+        if time > 0 {
+            time -= 1
+            timeLabel.text = String(time)
+        }
+        else {
+            timer.invalidate()
+        }
+    }
+    
+    @IBAction func playPressed(_ sender: Any) {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.decreaseTimer), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func pausePressed(_ sender: Any) {
+        timer.invalidate()
+    }
+    
+    
+    @IBAction func resetPressed(_ sender: Any) {
+        time = 210
+        timeLabel.text = String(time)
+    }
+    
+    
+    @IBAction func minusTen(_ sender: Any) {
+        time -= 10
+        timeLabel.text = String(time)
+    }
+    
+    
+    @IBAction func plusTen(_ sender: Any) {
+        time += 10
+        timeLabel.text = String(time)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        timeLabel.text = String(time)
     }
 
     override func didReceiveMemoryWarning() {
